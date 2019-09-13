d1792596b73ce0d65f1d690d87ab0c6a9556a492
Hadjime <GavrilovRoman@inbox.ru>
2019-09-13 16:07:04 +0700

    add script moving
    add new script Moving_2.

---
+++ Assets/Scripts/Player/Moving_2.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving_2 : MonoBehaviour
+{
+    [SerializeField]
+    float moveSpeed = 5;
+    [SerializeField]
+    bool isMoveUp = false;
+    [SerializeField]
+    bool isMoveDown = false;
+    [SerializeField]
+    bool isMoveRight = false;
+    [SerializeField]
+    bool isMoveLeft = false;
+
+    Rigidbody2D rb;
+    Vector2 movement;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
