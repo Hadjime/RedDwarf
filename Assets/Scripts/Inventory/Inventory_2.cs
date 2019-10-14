b49ffbd05c4541c7ae2b396ff28edc6c02a02872
Rasamaha <gavrilovroman@inbox.ru>
2019-10-14 14:58:56 +0700

    Инвентарь
    Добавление скриптов связаных с инвентарем.

---
+++ Assets/Scripts/Inventory/Inventory_2.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Inventory_2", menuName = "Scriptable Object/New Inventory_2", order = 51)]
+public class Inventory_2 : ScriptableObject
+{
+    public List<InventoryItem_2> items;
+}
+
+[System.Serializable]
+public class InventoryItem_2
+{
+    public string name;
+    public Sprite icon;
+    public int price;
+    public int attackDamage;
+}
+
+
