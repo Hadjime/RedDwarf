fb5f4228c20bed2c8e1887674f4fd01a302aa483
Hadjime <GavrilovRoman@inbox.ru>
2020-01-17 23:17:44 +0700

    Начал делать основное меню
    Сделал первый билд. Пакет Burst не давал нормально собрать проект. Удалил этот пакет. Появились новые ошибки. Пока не разобрался из-за чего.

--- Assets/InternalAssets/Scripts/_temp/LevelLoader.cs
+++ Assets/InternalAssets/Scripts/_temp/LevelLoader.cs
@@ -7,26 +7,16 @@ using UnityEngine.SceneManagement;
 public class LevelLoader : MonoBehaviour
 {
     public Animator transition;
-
     public float transitionTime = 1f;
 
-    // Update is called once per frame
-    void Update()
+    public void LoadNextLevel(string nameScene)
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            LoadNextLevel();
-        }
+        StartCoroutine(LoadLevel(nameScene));
     }
-
-    public void LoadNextLevel()
-    {
-        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
-    }
-    IEnumerator LoadLevel(int indexScene)
+    IEnumerator LoadLevel(string nameScene)
     {
         transition.SetTrigger("Start");
         yield return new WaitForSeconds(transitionTime);
-        SceneManager.LoadScene(indexScene);
+        SceneManager.LoadScene(nameScene);
     }
 }
