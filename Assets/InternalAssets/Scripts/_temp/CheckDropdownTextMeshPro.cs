a129efa43f825f68d45824b6c0388a80f116991b
Hadjime <GavrilovRoman@inbox.ru>
2020-01-19 01:07:42 +0700

    Добавил библиотеку DOTween и доделал выпадающий список
    В выпадающем списке прикрутил иконки языков

---
+++ Assets/InternalAssets/Scripts/_temp/CheckDropdownTextMeshPro.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace InternalAssets.Scripts._temp
+{
+    public class CheckDropdownTextMeshPro : MonoBehaviour
+    {
+        private TMP_Dropdown dropdown;
+
+        private void Start()
+        {
+            dropdown = GetComponent<TMP_Dropdown>();
+        }
+
+        public void Update()
+        {
+            var dOptions = dropdown.value;
+            Debug.Log("Value = " + dOptions);
+        }
+    }
+}
---
+++ Assets/InternalAssets/Scripts/_temp/MoveMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+public class MoveMenu : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.DOMove(Vector3.down, 2, false);
+    }
+}
