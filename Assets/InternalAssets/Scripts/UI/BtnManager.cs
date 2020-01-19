06ca875c12955ee628838a1387f8963c9fa8b887
Hadjime <GavrilovRoman@inbox.ru>
2020-01-19 14:18:07 +0700

    Создал класс менеджер UI кнопок.
    Клас отвечает за все кнопки UI интерфейса

---
+++ Assets/InternalAssets/Scripts/UI/BtnManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Lean.Localization;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.SceneManagement;
+
+public class BtnManager : MonoBehaviour
+{
+    [Header("Загрузка уровня с переходом")]
+    public Animator transitionAnimator;
+    public float transitionTime = 1f;
+    
+    public void LoadNextLevelWithTransition(string nameScene)
+    {
+        StartCoroutine(LoadLevel(nameScene));
+    }
+    
+    
+    public void SetLanguageIndex(int indexLanguage)
+    {
+        switch (indexLanguage)
+        {
+            case 0:
+                LeanLocalization.CurrentLanguage = "Russian";
+                break;
+            case 1:
+                LeanLocalization.CurrentLanguage = "English";
+                break;
+            default:
+                break;
+        }
+    }
+    
+    IEnumerator LoadLevel(string nameScene)
+    {
+        transitionAnimator.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(nameScene);
+    }
+}
