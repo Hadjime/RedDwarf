e5cbccb2252d0ba68f0423cd438ebd846f3ba972
Hadjime <GavrilovRoman@inbox.ru>
2020-01-23 23:17:41 +0700

    Эксперимент с UI
    UI в отдельной сцене. Потом подгрузка его в действующую сцену.

---
+++ Assets/InternalAssets/Scripts/UI/ClickSelectController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickSelectController : MonoBehaviour
+{
+    [SerializeField] private Camera _camer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            var ray = _camer.ScreenPointToRay(Input.mousePosition);
+            Debug.DrawRay(ray.origin, ray.direction * 100f, Color.red, 1f);
+            if (Physics.Raycast(ray, out var hitInfo))
+            {
+                Debug.Log(hitInfo.transform.name);
+            }
+        }
+    }
+}
---
+++ Assets/InternalAssets/Scripts/UI/UILoader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UILoader : MonoBehaviour
+{
+
+    //<summary
+    //123
+    //</summary>
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            if (SceneManager.GetSceneByName("UIIndicators").isLoaded == false)
+            {
+                SceneManager.LoadSceneAsync("UIIndicators", LoadSceneMode.Additive);
+            }
+            else
+            {
+                SceneManager.UnloadSceneAsync("UIIndicators");
+            }
+        }
+    }
+}
