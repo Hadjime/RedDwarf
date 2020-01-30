78912dbe40f57a8b45e19f03435cbf1a32104b5a
Hadjime <GavrilovRoman@inbox.ru>
2020-01-30 22:41:23 +0700

    Добавил еще один вариант скроллинга.
    Скролл для смены карточек оружия в интерфейсе.

---
+++ Assets/InternalAssets/Scripts/_temp/MyListBox.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace InternalAssets.Scripts._temp
+{
+    public class MyListBox : BaseListBank
+    {
+        private int[] _contents = {
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+        };
+        private void Start()
+        {
+            //throw new NotImplementedException();
+        }
+
+        public override string GetListContent(int index)
+        {
+            return _contents[index].ToString();
+        }
+
+        public override int GetListLength()
+        {
+            return _contents.Length;
+        }
+    }
+}
