1e3414a27927c51d21fcfdbb389ad28307e51e54
Hadjime <GavrilovRoman@inbox.ru>
2020-01-31 22:13:52 +0700

    Добавил пакет Inpit System
    Новая система ввода в Unity. Появился начиная с версии 2019.3.
    Создал сцену и приспособил эту новую систему ввода.

---
+++ Assets/InternalAssets/Scripts/_temp/CuberController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace InternalAssets.Scripts._temp
+{
+    public class CuberController : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+
+        public void OnMoveUp()
+        {
+            transform.Translate(transform.up);
+        }
+        public void OnMoveDown()
+        {
+            transform.Translate(-transform.up);
+        }
+        public void OnMoveRight()
+        {
+            transform.Translate(transform.right);
+        }
+        public void OnMoveLeft()
+        {
+            transform.Translate(-transform.right);
+        }
+    }
+}
