1832a5948f8b50cfb40789c574e7859880a0de75
Hadjime <GavrilovRoman@inbox.ru>
2020-03-05 22:50:51 +0700

    Добавил тумайн войны
    Туман войны через тайловые клетки.

---
+++ Assets/InternalAssets/Scripts/Player/DestroyFogOfWar.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class DestroyFogOfWar : MonoBehaviour
+{
+    [SerializeField] private Grid grid;
+    [SerializeField] private Tilemap fogOfWar;
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        foreach (var contact in other.contacts)
+        {
+            Vector3Int point = grid.WorldToCell(contact.point);
+            fogOfWar.SetTile(point, null);
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        foreach (var contact in other.contacts)
+        {
+            Vector3Int point = grid.WorldToCell(contact.point);
+            fogOfWar.SetTile(point, null);
+        }
+    }
+}
