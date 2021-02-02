7fc5c27f603ccd4fdae071c2bee4e836cd422fe1
Hadjime <GavrilovRoman@inbox.ru>
2021-02-02 22:41:23 +0700

    Added control player throught Finit State Machine

---
+++ Assets/InternalAssets/Scripts/Player/PlayerFinitStateMachine/PlayerFSM.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine.InputSystem.LowLevel;
+
+namespace InternalAssets.Scripts.Player.PlayerFinitStateMachine
+{
+    public class PlayerFSM
+    {
+        public PlayerState CurrentState { get; private set; }
+
+        public void Initialize(PlayerState startingState)
+        {
+            CurrentState = startingState;
+            CurrentState.Enter();
+        }
+
+        public void ChangeState(PlayerState newState)
+        {
+            CurrentState.Exit();
+            CurrentState = newState;
+            CurrentState.Enter();
+        }
+    }
+}
---
+++ Assets/InternalAssets/Scripts/Player/PlayerFinitStateMachine/PlayerState.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace InternalAssets.Scripts.Player.PlayerFinitStateMachine
+{
+    public abstract class PlayerState
+    {
+        protected PlayerFSM PlayerFsm;
+        
+        protected PlayerState(PlayerFSM playerFsm)
+        {
+            this.PlayerFsm = playerFsm;
+        }
+
+        public virtual void Enter()
+        {
+            
+        }
+
+        public virtual void Exit()
+        {
+            
+        }
+
+        public virtual void LogicUpdate()
+        {
+            
+        }
+
+        public virtual void PhysicsUpdate()
+        {
+            
+        }
+    }
+}
