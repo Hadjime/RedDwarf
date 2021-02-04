51fb5348c3b7be3fcba2891e33c20e7efae26446
Rasamaha <gavrilovroman@inbox.ru>
2021-02-04 17:07:14 +0700

    added PlayerInputHandler.cs for move

---
+++ Assets/InternalAssets/Scripts/Player/Input/PlayerInputHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace InternalAssets.Scripts.Player.Input
+{
+    [RequireComponent(typeof(PlayerInput))]
+    public class PlayerInputHandler : MonoBehaviour
+    {
+        private PlayerInput _playerInput;
+
+        public Vector2 RawMovementInput { get; private set; };
+
+        public void Awake()
+        {
+            _playerInput = GetComponent<PlayerInput>();
+        }
+
+        public void Move(InputAction.CallbackContext context)
+        {
+            RawMovementInput = context.ReadValue<Vector2>();
+        }
+    }
+}
