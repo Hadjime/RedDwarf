28e8cc70572d0274719f90cbed4cc2f9668fd9f9
Rasamaha <gavrilovroman@inbox.ru>
2021-02-05 14:30:33 +0700

    added test class state

---
+++ Assets/InternalAssets/Scripts/Player/PlayerStates/PlayerTestState.cs
@@ -0,0 +1,33 @@
+﻿using InternalAssets.Scripts.Player.Data;
+using UnityEngine;
+using InternalAssets.Scripts.Player.PlayerFinitStateMachine;
+
+namespace InternalAssets.Scripts.Player.PlayerStates
+{
+    public class PlayerTestState : PlayerState
+    {
+        public PlayerTestState(PlayerFinitStateMachine.Player player, PlayerFSM playerFsm, PlayerData playerData, string animBoolName) : base(player, playerFsm, playerData, animBoolName)
+        {
+        }
+
+        public override void Enter()
+        {
+            base.Enter();
+        }
+
+        public override void Exit()
+        {
+            base.Exit();
+        }
+
+        public override void LogicUpdate()
+        {
+            base.LogicUpdate();
+        }
+
+        public override void PhysicsUpdate()
+        {
+            base.PhysicsUpdate();
+        }
+    }
+}
