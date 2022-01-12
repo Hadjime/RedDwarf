05223d24ac9588c81aa5b25f6179921081701fb9
Roma <123@123.com>
2022-01-13 01:04:20 +0700

    Addition GameBootstrapper in scene and attempt create inputservice

---
+++ Assets/InternalAssets/Scripts/Infrastructure/Game.cs
@@ -0,0 +1,18 @@
+﻿using InternalAssets.Scripts.Services.Input;
+
+namespace InternalAssets.Scripts.Infrastructure
+{
+    public class Game
+    {
+        public static IInputService InputServices;
+        public Game()
+        {
+            RegisterInputServices();
+        }
+
+        private void RegisterInputServices()
+        {
+            InputServices = new InputService();
+        }
+    }
+}
---
+++ Assets/InternalAssets/Scripts/Services/Input/InputService.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace InternalAssets.Scripts.Services.Input
+{
+    public class InputService : IInputService
+    {
+        public Vector2 RawMovementInput { get; }
+    }
+}
