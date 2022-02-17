61df11e1980df7aa6eabab74d27c94517853b167
Roma <123@123.com>
2022-02-18 00:57:46 +0700

    - addition init SRDebugger in BootstrapState

---
+++ Assets/InternalAssets/Scripts/Cheats/CheatsThroughDI.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using InternalAssets.Scripts.Data;
+using InternalAssets.Scripts.Infrastructure.Services.PersistentProgress;
+
+namespace InternalAssets.Scripts.Cheats
+{
+    public class CheatsThroughDI
+    {
+        private readonly IPersistentProgressService _progressService;
+        public static readonly CheatsThroughDI Instance;
+
+        public CheatsThroughDI(IPersistentProgressService progressService)
+        {
+            _progressService = progressService;
+        }
+
+        [Category("LOOT")]
+        public void Add_100_Gold()
+        {
+            _progressService.Progress.WorldData.LootData.Collect(new Loot(){Value = 100});
+        }
+    }
+}
