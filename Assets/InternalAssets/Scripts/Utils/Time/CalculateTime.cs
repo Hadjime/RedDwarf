39bc0ca99d9c9a9f96c3c3623814d349619ff196
HadjimeSun <Gavrilov@taketopgames.com>
2022-06-26 16:19:33 +0700

    - пример нативного класса для работы со временем, потом можно на это выстроить честную локальную проверку времени

---
+++ Assets/InternalAssets/Scripts/Utils/Time/CalculateTime.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+
+namespace InternalAssets.Scripts.Utils.Time
+{
+	public class CalculateTime
+	{
+		//TODO из нативного класса SystemClock можно получить время не привяззанное к времени наустройстве, которое игрок может самостоятельно поменять
+		public static long ElapsedTime()
+        		{
+        			if (Application.platform != RuntimePlatform.Android) return 0;
+        			AndroidJavaClass systemClock = new AndroidJavaClass("android.os.SystemClock");
+        			return systemClock.CallStatic<long>("elapsedRealtime");
+        		}
+	}
+}
